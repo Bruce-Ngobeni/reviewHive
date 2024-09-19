278c073c0baa68b0ae65915573569cf799543618
Bruce-Ngobeni <brucengobeni2005@gmail.com>
2024-09-19 15:28:21 +0200

    Added app.js file, and installed express, node-fetch, and ejs dependencies

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from "express";
+import fetch from "node-fetch";
+
+const app = express();
+const PORT = 3000;
+
+
+app.listen(PORT, () => {
+    console.log("Server is up and running on port: ", PORT);
+})
+
