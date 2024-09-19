10979d9f445fb4b29348555fa9464df6e879edbe
Bruce-Ngobeni <brucengobeni2005@gmail.com>
2024-09-19 15:30:29 +0200

    Configed the app to use ejs as a search engine

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ import fetch from "node-fetch";
 const app = express();
 const PORT = 3000;
 
+app.set("view engine", "ejs");
+
+
 
 app.listen(PORT, () => {
     console.log("Server is up and running on port: ", PORT);
