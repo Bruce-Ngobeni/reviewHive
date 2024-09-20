8b72e55247b8a0988db9152d3b04a15700e78fd8
Bruce-Ngobeni <brucengobeni2005@gmail.com>
2024-09-20 08:47:27 +0200

    Added an dummy array of comics objects

--- app.mjs
+++ app.mjs
@@ -8,6 +8,24 @@ const PORT = 3000;
 app.set("view engine", "ejs");
 
 
+const comics = [
+    {
+        title:"Watchmen",
+        description: "I'm baby heirloom cold-pressed venmo prism. Mumblecore iceland cloud bread, semiotics swag disrupt waistcoat solarpunk. Forage fam big mood flannel, pickled bodega boys poutine selfies tote bag. Tacos health goth selfies leggings aesthetic venmo. Meditation hexagon cronut DIY quinoa aesthetic synth keytar street art before they sold out sus waistcoat. JOMO schlitz godard farm-to-table gluten-free distillery vibecession blog enamel pin tumeric master cleanse selvage put a bird on it.",
+        image: "https://www.imdb.com/title/tt0409459/mediaviewer/rm2931438848/?ref_=tt_ov_i"
+    },
+    {
+        title:"Batman",
+        description: "Snackwave sustainable meh banjo poutine +1 williamsburg tote bag locavore pop-up. Raw denim kickstarter XOXO actually, seitan celiac hoodie Brooklyn +1. Snackwave cliche heirloom humblebrag, gluten-free shabby chic XOXO meggings mixtape seitan banh mi. Lo-fi lyft meditation fashion axe, meh twee same. Sartorial etsy deep v, palo santo 8-bit gluten-free pickled offal selvage same bodega boys whatever vice shoreditch YOLO. Vibecession poke bespoke squid vinyl try-hard pok pok drinking vinegar fanny pack lumbersexual mukbang. Prism succulents meditation pour-over tacos mlkshk PBR&B.",
+        image: "https://x.com/Batman/photo"
+    },
+    {
+        title:"Dragon Ball",
+        description: "Sartorial cold-pressed williamsburg, listicle flannel 90's hashtag lomo marfa everyday carry single-origin coffee 8-bit occupy JOMO fanny pack. Chillwave tonx paleo tilde cardigan beard. Chartreuse knausgaard live-edge gatekeep occupy retro. Pop-up chicharrones hella, copper mug before they sold out gatekeep live-edge trust fund artisan. Letterpress beard neutral milk hotel truffaut crucifix. Tacos mumblecore banh mi four loko man bun lo-fi jean shorts synth scenester hoodie JOMO dreamcatcher fixie mixtape DIY. Meditation cred bodega boys man braid aesthetic gorpcore fingerstache beard mumblecore praxis hammock jianbing celiac.",
+        image: "https://deadline.com/wp-content/uploads/2024/07/Dragon-Ball-Series_c42772.jpg?w=681&h=383&crop=1"
+    }
+]
+
 app.get("/", (req, res) => {
     res.render("landing");
 })
