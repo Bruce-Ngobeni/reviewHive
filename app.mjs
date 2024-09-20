45e78debbccfc76168d1782bcf9e9b41de761338
Bruce-Ngobeni <brucengobeni2005@gmail.com>
2024-09-20 08:34:22 +0200

    Added comics page and the comics route

--- app.mjs
+++ app.mjs
@@ -1,4 +1,5 @@
 import express from "express";
+// const express = require("express")
 import fetch from "node-fetch";
 
 const app = express();
@@ -6,6 +7,14 @@ const PORT = 3000;
 
 app.set("view engine", "ejs");
 
+
+app.get("/", (req, res) => {
+    res.render("landing");
+})
+
+app.get("/comics", (req, res) => {
+    res.render("comics")
+})
 
 
 app.listen(PORT, () => {
