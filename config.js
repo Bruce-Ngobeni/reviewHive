473c9b2f5b6ed3950ac9b51b00f1ba9a2996e5d6
Bruce-Ngobeni <brucengobeni2005@gmail.com>
2024-09-23 15:35:28 +0200

    deleted the config.rfig.js file

--- config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    db: {
-        username: "brucengobeni2005",
-        connection: "mongodb+srv://brucengobeni2005:Dzunisani@99@appdb.kwvwj.mongodb.net/?retryWrites=true&w=majority&appName=appDB"
-    }
-};
-
-
-module.exports = config;
