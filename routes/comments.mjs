f76fd87fecac00060bd463fb306099377d5b676a
Bruce <brucengobeni2005@gmail.com>
2025-01-09 17:49:46 +0200

    Added a comments_edit.ejs view for editing comments. Implemented the edit route to handle comment updates and the delete route for removing comments.

--- routes/comments.mjs
+++ routes/comments.mjs
@@ -1,9 +1,8 @@
 import express from "express";
 const router = express.Router({mergeParams: true});
 import Comment from "../models/comment.js";
-import req from "express/lib/request.js";
 import Comic from "../models/comic.js";
-import comment from "../models/comment.js";
+import req from "express/lib/request.js";
 
 
 // New comment - show form
@@ -40,6 +39,9 @@ router.get("/:commentId/edit", async (req, res) => {
         const comic = await Comic.findById(req.params.id).exec();
         const comment = await Comment.findById(req.params.commentId).exec();
 
+        console.log("Comic:", comic);
+        console.log("Comment:", comment);
+
         res.render("comments_edit", {comic, comment});
     }catch (err) {
         console.log(err);
@@ -47,5 +49,31 @@ router.get("/:commentId/edit", async (req, res) => {
     }
 })
 
+
+// Update Comment - update in DB
+router.put("/:commentId", async (req, res) => {
+    try {
+        const comment = await Comment.findByIdAndUpdate(req.params.commentId, {text:req.body.text}, {new:true})
+        console.log(comment);
+        res.redirect(`/comics/${req.params.id}`);
+    } catch(err) {
+        console.log(err);
+        res.send("Brokeeeeeee comment PUT");
+    }
+})
+
+
+// Delete comment
+router.delete("/:commentId", async (req, res) => {
+    try {
+        const comment = await Comment.findByIdAndDelete(req.params.commentId);
+        console.log(comment);
+        res.redirect(`/comics/${req.params.id}`);
+        
+    } catch (err) {
+        console.log(err);
+        res.send("Broken comment DELETE")
+    }
+})
 
 export default router;
